ba730b49a3a90f5f7ab2c04cecc10101e3a3238e
lwisekiml <lwisekiml@naver.com>
2016-07-10 23:19:11 +0900

    6_namespace.cpp complete

--- 6_namespace.cpp
+++ 6_namespace.cpp
@@ -2,10 +2,50 @@
 
 using namespace std;
 
+namespace DemoLib
+{
+  struct Stack
+  {
+    int top;
+  };
+  struct Queue
+  {
+    int front; 
+    int rear;
+  };
+}
+using namespace DemoLib;
+/*
+namespace DemoA
+{
+  struct Stack
+  {
+    int top;
+  };
+}
 
-
+namespace DemoB
+{
+  struct Stack
+  {
+    int last;
+  };
+}
+*/
 int main(void)
 {
+  Stack s;
+  Queue q;
+  s.top = -1;
+  q.front = q.rear = 0;
+  /*
+  DemoA::Stack stacka;
+  DemoB::Stack stackb;
 
+  stacka.top = -1;
+  stackb.last = -1;
+*/
+  cout<<s.top<<endl;
+  
   return 0;
 }
